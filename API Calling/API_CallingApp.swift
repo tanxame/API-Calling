7e341b0de4f3f2cf6b4353718d09eb5421f9eab7
Amelia <atan@ucls.uchicago.edu>
2021-07-29 17:08:27 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Franklin Tan on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
